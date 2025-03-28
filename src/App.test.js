0cd42fc611dc91d986a69752948ee46b2f430049
gaconza <gabrielzacharias@yahoo.com.br>
2025-03-27 20:44:27 -0600

    Added App.test.js for unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders my name on the page', () => {
   render(<App />);
-  const nameElement = screen.getByText(/My name is Gabriel Zacharias/i);
+  const nameElement = screen.getByText(/My name is Gabriel Zacharias/i); 
   expect(nameElement).toBeInTheDocument();
 });
